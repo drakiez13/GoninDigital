4c194a50f6633e6bc9d156e85b66534edce1d488
Huy Chau W <chaungochuy0709@gmail.com>
2021-10-02 10:30:15 +0700

    Add Dashbord view

--- Views/DashBoard.xaml.cs
+++ Views/DashBoard.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace GoninDigital.Views
@@ -17,7 +18,7 @@ namespace GoninDigital.Views
     /// <summary>
     /// Interaction logic for DashBoard.xaml
     /// </summary>
-    public partial class DashBoard : Window
+    public partial class DashBoard : UserControl
     {
         public DashBoard()
         {
