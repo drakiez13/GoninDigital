ac16f354212c7bf81c35399900028663db5ad8b5
Huy Chau W <chaungochuy0709@gmail.com>
2021-11-10 00:00:46 +0700

    merge entry into admin

---
+++ Views/AdminPages/UserPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GoninDigital.Models;
+
+namespace GoninDigital.Views.AdminPages
+{
+    /// <summary>
+    /// Interaction logic for UserPage.xaml
+    /// </summary>
+    public partial class UserPage
+    {
+        public UserPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
