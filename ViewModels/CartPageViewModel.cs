a0719714224709ca4d989e01708046b6c6a9cfc6
C-Minh <sater5201@gmail.com>
2021-11-16 00:11:23 +0700

    Test CartPageVM

---
+++ ViewModels/CartPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoninDigital.Models;
+
+namespace GoninDigital.ViewModels
+{
+    class CartPageViewModel :BaseViewModel
+    {
+        
+
+        private List<Product> recommnededByEditor;
+        public List<Product> RecommendedByEditor
+        {
+            get { return recommnededByEditor; }
+            set { recommnededByEditor = value; OnPropertyChanged(); }
+        }
+        public List<Product> RecommendedByEditor3
+        {
+            get { return recommnededByEditor.GetRange(0, 3); }
+        }
+
+        public CartPageViewModel()
+        {
+  
+            GoninDigitalDBContext db = DataProvider.Instance.Db;
+            recommnededByEditor = db.Products.ToList();
+        }
+    }
+}
