9130cdb60e762dfe3cc001245c0a580c788da138
Thang Tran <drakiez92@gmail.com>
2021-11-16 15:01:57 +0700

    Product card navigates to product page

---
+++ ViewModels/ProductPageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GoninDigital.ViewModels
+{
+    class ProductPageViewModel : BaseViewModel
+    {
+        private int productId;
+        public int ProductId
+        {
+            get => productId;
+            set { productId = value; OnPropertyChanged(); }
+        }
+
+        public ProductPageViewModel()
+        {
+
+        }
+    }
+}
