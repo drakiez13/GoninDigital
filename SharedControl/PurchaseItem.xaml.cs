ec4481596e98f1d4624eed7f63b8e692d01be9e4
C-Minh <sater5201@gmail.com>
2021-11-17 23:57:12 +0700

    Add Purchase view and viewmodel

--- SharedControl/PurchaseItem.xaml.cs
+++ SharedControl/PurchaseItem.xaml.cs
@@ -20,6 +20,37 @@ namespace GoninDigital.SharedControl
     /// </summary>
     public partial class PurchaseItem : UserControl
     {
+        public object Title
+        {
+            get => (object)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public object Delivery
+        {
+            get => (object)GetValue(DeliveryProperty);
+            set => SetValue(DeliveryProperty,value);
+        }
+        public object Price
+        {
+            get => (object)GetValue(PriceProperty);
+            set => SetValue(PriceProperty, value);
+        }
+        public object TotalPrice
+        {
+            get => (object)GetValue(TotalPriceProperty);
+            set => SetValue(TotalPriceProperty, value);
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(object), typeof(PurchaseItem), new PropertyMetadata("Unknown"));
+
+        public static readonly DependencyProperty DeliveryProperty =
+            DependencyProperty.Register("Delivery", typeof(object), typeof(PurchaseItem), new PropertyMetadata(50.000));
+        public static readonly DependencyProperty PriceProperty =
+            DependencyProperty.Register("Price", typeof(object), typeof(PurchaseItem), new PropertyMetadata(0));
+        public static readonly DependencyProperty TotalPriceProperty =
+            DependencyProperty.Register("TotalPrice", typeof(object), typeof(PurchaseItem), new PropertyMetadata(0));
         public PurchaseItem()
         {
             InitializeComponent();
