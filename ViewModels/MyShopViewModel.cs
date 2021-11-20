bf10a788c027ad56bd05dc17a948019a5a8339a6
C-Minh <sater5201@gmail.com>
2021-11-20 22:32:25 +0700

    Myshop viewmodel

---
+++ ViewModels/MyShopViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using GoninDigital.Models;
+
+namespace GoninDigital.ViewModels
+{
+    class MyShopViewModel:BaseViewModel
+    {
+        private string avatar;
+        public string Avatar
+        {
+            get { return avatar; }
+            set { avatar = value; OnPropertyChanged(); }
+        }
+        private string avatarBehind;
+        public string AvatarBehind
+        {
+            get { return avatarBehind; }
+            set { avatarBehind = value; OnPropertyChanged(); }
+        }
+
+        private List<Product> recommnededByEditor;
+        public List<Product> RecommendedByEditor
+        {
+            get { return recommnededByEditor; }
+            set { recommnededByEditor = value; OnPropertyChanged(); }
+        }
+        public List<Product> RecommendedByEditor3
+        {
+            get { return recommnededByEditor.GetRange(0, 3); }
+        }
+
+        
+
+        public MyShopViewModel()
+        {
+            GoninDigitalDBContext db = DataProvider.Instance.Db;
+            recommnededByEditor = db.Products.ToList();
+        }
+    }
+}
