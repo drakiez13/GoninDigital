3505a4bcd64fb7ccca4f51c6cceace11f4194cf0
C-Minh <sater5201@gmail.com>
2021-11-21 23:47:02 +0700

    Myshop vm

--- ViewModels/MyShopViewModel.cs
+++ ViewModels/MyShopViewModel.cs
@@ -63,46 +63,6 @@ namespace GoninDigital.ViewModels
                           where pro.vendorid
 
         }
-        private string mail;
-        public string Mail
-        {
-            get { return mail; }
-            set { mail = value; OnPropertyChanged(); }
-        }
-        private string phone;
-        public string Phone
-        {
-            get { return phone; }
-            set { phone = value; OnPropertyChanged(); }
-        }
-        private string address;
-        public string Address
-        {
-            get { return address; }
-            set { address = value; OnPropertyChanged(); }
-        }
-        private string description;
-        public string Description
-        {
-            get { return description; }
-            set { description = value; OnPropertyChanged(); }
-        }
-        private List<Product> productList;
-        public List<Product> ProductList
-        {
-            get { return productList; }
-            set { productList = value; OnPropertyChanged(); }
-        }
 
-
-
-
-        public MyShopViewModel()
-        {
-            GoninDigitalDBContext db = DataProvider.Instance.Db;
-            productList = from pro in Product
-                          where pro.vendorid
-
-        }
     }
 }
