c119c84cae2a2bff61cd6a7849cbfbcf1f3d02bd
C-Minh <sater5201@gmail.com>
2021-11-22 20:09:55 +0700

    WIP shopproduct

--- SharedControl/ShopProduct.xaml.cs
+++ SharedControl/ShopProduct.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GoninDigital.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,21 @@ namespace GoninDigital.SharedControl
     /// </summary>
     public partial class ShopProduct : UserControl
     {
+        public object ProductList
+        {
+            get => (object)GetValue(ProductListProperty);
+            set => SetValue(ProductListProperty, value);
+        }
+        private static List<Product> metaProducts = new List<Product> {
+            new Product { Name="Product 1", Price=100000},
+            new Product { Name="Product 2", Price=200000},
+            new Product { Name="Product 3", Price=300000},
+            new Product { Name="Product 4", Price=400000},
+            new Product { Name="Product 5", Price=400000}
+        };
+
+        public static readonly DependencyProperty ProductListProperty =
+            DependencyProperty.Register("ProductList", typeof(object), typeof(ShopProduct), new PropertyMetadata(metaProducts));
         public ShopProduct()
         {
             InitializeComponent();
