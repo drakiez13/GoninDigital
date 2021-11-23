a15e1d7c6a37c7fddd598075bc88021492a9fccd
Minh Phan <minh2442001@gmail.com>
2021-11-24 04:03:14 +0700

    add process ring

--- Views/LoginView.xaml.cs
+++ Views/LoginView.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GoninDigital.ViewModels;
+using ModernWpf.Demo.ThreadedUI;
 
 namespace GoninDigital.Views
 {
@@ -23,6 +25,37 @@ namespace GoninDigital.Views
         public LoginView()
         {
             InitializeComponent();
+        }
+
+        public ThreadedUIPageViewModel ViewModel { get; } = new ThreadedUIPageViewModel();
+
+        private void ProgressControlHost_ChildChanged(object sender, EventArgs e)
+        {
+            var host = (ThreadedVisualHost)sender;
+            if (host.Child is ThreadedProgressBar progressBar)
+            {
+                progressBar.SetBinding(ThreadedProgressBar.IsIndeterminateProperty, new Binding(nameof(ViewModel.IsBusy)) { Source = ViewModel });
+            }
+            else if (host.Child is ThreadedProgressRing progressRing)
+            {
+                progressRing.SetBinding(ThreadedProgressRing.IsActiveProperty, new Binding(nameof(ViewModel.IsBusy)) { Source = ViewModel });
+            }
+        }
+    }
+    public class ThreadedUIPageViewModel : BaseViewModel
+    {
+        private bool _isBusy = true;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => Set(ref _isBusy, value);
+        }
+
+        private bool _isVisbile = true;
+        public bool IsVisible
+        {
+            get => _isVisbile;
+            set => Set(ref _isVisbile, value);
         }
     }
 }
