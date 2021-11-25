ce2a42be844b0b495df9df7e856cdfb60575aab3
Thang Tran <drakiez92@gmail.com>
2021-11-25 13:11:32 +0000

    Base functions of homepage just work

--- ViewModels/HomePageViewModel.cs
+++ ViewModels/HomePageViewModel.cs
@@ -25,33 +25,79 @@ namespace GoninDigital.ViewModels
             set { recommendedByEditor = value; OnPropertyChanged(); }
         }
 
-        private string artGroup1;
-        public string ArtGroup1
+        List<Ad> ads = null;
+        public List<Ad> Ads
         {
-            get => artGroup1;
+            get { return ads; }
+            set { ads = value; OnPropertyChanged(); }
         }
 
-        private string artGroup2;
-        public string ArtGroup2
+        private List<List<Product>> adProducts = null;
+        public List<List<Product>> AdProducts
         {
-            get => artGroup2;
+            get { return adProducts; }
+            set { adProducts = value; OnPropertyChanged(); }
         }
 
-        private void Init()
+        private List<Product> topProducts = null;
+        public List<Product> TopProducts
+        {
+            get { return topProducts; }
+            set { topProducts = value; OnPropertyChanged(); }
+        }
+
+        private List<Product> recommendedProducts = null;
+        public List<Product> RecommendedProducts
+        {
+            get { return recommendedProducts; }
+            set { recommendedProducts = value; OnPropertyChanged(); }
+        }
+
+        private List<Product> discountProducts = null;
+        public List<Product> DiscountProducts
+        {
+            get { return discountProducts; }
+            set { discountProducts = value; OnPropertyChanged(); }
+        }
+
+        private void InitAds()
         {
             using (var db = new GoninDigitalDBContext())
             {
-                RecommendedByEditor = db.Products.ToList();
+                Ads = db.Ads.OrderBy(o => Guid.NewGuid()).Take(3).ToList();
+                List<List<Product>> _adProducts = new List<List<Product>>(3);
+                for (int i = 0; i < 3; i++)
+                {
+                    _adProducts.Add(db.AdDetails.Where(o => o.AdId == Ads[i].Id).Select(ob => ob.Product).ToList());
+
+                }
+                AdProducts = _adProducts;
+            }
+        }
+
+        private void InitProducts()
+        {
+            using (var db = new GoninDigitalDBContext())
+            {
+                // Selection algorithm goes here
+                TopProducts = db.Products.OrderBy(o => Guid.NewGuid()).Take(6).ToList();
+
+                RecommendedProducts = db.Products.OrderBy(o => Guid.NewGuid()).Take(6).ToList();
+
+                DiscountProducts = db.Products.OrderBy(o => Guid.NewGuid()).Take(6).ToList();
             }
         }
 
         public HomePageViewModel()
         {
             art = "/Resources/Images/HomeBanner.jpg";
-            artGroup1 = "/Resources/Images/HomeProductCardGroupBackground.png";
-            artGroup2 = "/Resources/Images/HomeProductCardGroupBackground2.jpg";
-            Thread thread = new Thread(Init);
-            thread.Start();
+
+            ads = new List<Ad>(3);
+            adProducts = new List<List<Product>>(3);
+            Thread thread1 = new Thread(InitAds);
+            thread1.Start();
+            Thread thread2 = new Thread(InitProducts);
+            thread2.Start();
         }
     }
 }
