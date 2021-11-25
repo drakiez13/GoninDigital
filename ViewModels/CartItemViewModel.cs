3c956a032e2893c3475a7179cf37baea3275549c
C-Minh <sater5201@gmail.com>
2021-11-25 23:10:51 +0700

    fix cartpage

--- ViewModels/CartItemViewModel.cs
+++ ViewModels/CartItemViewModel.cs
@@ -11,41 +11,13 @@ namespace GoninDigital.ViewModels
 {
     class CartItemViewModel:BaseViewModel
     {
-        /*private object _Title;
-        public object Title
-        {
-            get { return _Title; }
-            set { _Title = value; OnPropertyChanged(); }
-        }
-        private object _Image;
-        public object Image
-        {
-            get { return _Image; }
-            set { _Image = value; OnPropertyChanged(); }
-        }
-
-        private object _Price;
-        public object Price
-        {
-            get { return _Price; }
-            set { _Price = value; OnPropertyChanged(); }
-        }
-        private object _TotalPrice;
-        public object TotalPrice
-        {
-            get { return _TotalPrice; }
-            set { _TotalPrice = value; OnPropertyChanged(); }
-        }
-*/
-
-       
-        
+            
         public CartItemViewModel()
         {
-            RemoveCartItem = new RelayCommand<object>((p) => { return true; }, (p) =>
+           /* RemoveCartItem = new RelayCommand<object>((p) => { return true; }, (p) =>
              {
                  MessageBox.Show("adsad");
-             });
+             });*/
         }
     }
 }
