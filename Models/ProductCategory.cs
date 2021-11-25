1ef93b9e223b09a60a22990aa889b9767a910dac
Thang Tran <drakiez92@gmail.com>
2021-11-25 13:39:39 +0500

    update db models

--- Models/ProductCategory.cs
+++ Models/ProductCategory.cs
@@ -9,12 +9,14 @@ namespace GoninDigital.Models
     {
         public ProductCategory()
         {
+            ProductSpecs = new HashSet<ProductSpec>();
             Products = new HashSet<Product>();
         }
 
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public virtual ICollection<ProductSpec> ProductSpecs { get; set; }
         public virtual ICollection<Product> Products { get; set; }
     }
 }
