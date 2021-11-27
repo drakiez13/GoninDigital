df131a190d48fad65263632e0300014b947f98f1
Huy Chau W <chaungochuy0709@gmail.com>
2021-11-27 19:44:42 +0700

    user page view

--- ViewModels/UserSettingViewModel.cs
+++ ViewModels/UserSettingViewModel.cs
@@ -17,93 +17,105 @@ namespace GoninDigital.ViewModels
     class UserSettingViewModel : BaseViewModel
     {
         #region Properties
-        private bool _flag;
-        public bool flag
+        private bool flag;
+        public bool Flag
         {
-            get { return _flag; }
-            set { _flag = value; OnPropertyChanged("flag"); }
+            get { return flag; }
+            set { flag = value; OnPropertyChanged("Flag"); }
         }
-        private string _flag1;
-        public string flag1
+        private string flag1;
+        public string Flag1
         {
-            get { return _flag1; }
-            set { _flag1 = value; OnPropertyChanged("flag1"); }
+            get { return flag1; }
+            set { flag1 = value; OnPropertyChanged("Flag1"); }
         }
-        private string _flag2;
-        public string flag2
+        private string flag2;
+        public string Flag2
         {
-            get { return _flag2; }
-            set { _flag2 = value; OnPropertyChanged("flag2"); }
+            get { return flag2; }
+            set { flag2 = value; OnPropertyChanged("Flag2"); }
         }
-        private bool _flag3;
-        public bool flag3
+        private bool flag3;
+        public bool Flag3
         {
-            get { return _flag3; }
-            set { _flag3 = value; OnPropertyChanged("flag3"); }
+            get { return flag3; }
+            set { flag3 = value; OnPropertyChanged("Flag3"); }
         }
-        private List<String> _lGender;
-        public List<String> lGender
+        private List<String> lGender;
+        public List<String> LGender
         {
             get { return new List<string>() { "Other", "Female", "Male" }; }
         }
-        private List<String> _lUserType;
-        public List<String> lUserType
+        private List<String> lUserType;
+        public List<String> LUserType
         {
             get { return new List<string>() { "Admin", "Vendor", "Customer" }; }
         }
-        private string _id;
+        private string id;
         public string Id
         {
-            get { return _id; }
-            set { _id = value; OnPropertyChanged("Id"); }
+            get { return id; }
+            set { id = value; OnPropertyChanged("Id"); }
         }
-        private string _gender;
+        private string gender;
         public string Gender
         {
-            get { return _gender; }
-            set { _gender = value; OnPropertyChanged("Gender"); }
+            get { return gender; }
+            set { gender = value; OnPropertyChanged("Gender"); }
         }
-        private string _UserType_index;
+        private string userType_index;
         public string UserType_index
         {
-            get { return _UserType_index; }
-            set { _UserType_index = value; OnPropertyChanged("UserType_index"); }
+            get { return userType_index; }
+            set { userType_index = value; OnPropertyChanged("UserType_index"); }
         }
-        private string _userType;
-        public string userType
+        private string userType;
+        public string UserType
         {
-            get { return _userType; }
-            set { _userType = value; OnPropertyChanged("userType"); }
+            get { return userType; }
+            set { userType = value; OnPropertyChanged("UserType"); }
         }
-        private string _firstName;
+        private string firstName;
         public string FirstName
         {
-            get { return _firstName; }
-            set { _firstName = value; OnPropertyChanged("FirstName"); }
+            get { return firstName; }
+            set { firstName = value; OnPropertyChanged("FirstName"); }
         }
-        private string _lastName;
+        private string fullName;
+        public string FullName
+        {
+            get { return fullName; }
+            set { fullName = value; OnPropertyChanged("FullName"); }
+        }
+        private string userName;
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; OnPropertyChanged("UserName"); }
+        }
+        private string lastName;
         public string LastName
         {
-            get { return _lastName; }
-            set { _lastName = value; OnPropertyChanged("LastName"); }
+            get { return lastName; }
+            set { lastName = value; OnPropertyChanged("LastName"); }
         }
-        private string _phoneNum;
+        private string phoneNum;
         public string PhoneNum
         {
-            get { return _phoneNum; }
-            set { _phoneNum = value; OnPropertyChanged("PhoneNum"); }
+            get { return phoneNum; }
+            set { phoneNum = value; OnPropertyChanged("PhoneNum"); }
         }
-        private string _email;
+        private string email;
         public string Email
         {
-            get { return _email; }
-            set { _email = value; OnPropertyChanged("Email"); }
+            get { return email; }
+            set { email = value; OnPropertyChanged("Email"); }
         }
-        private DateTime _dob;
+        private DateTime dob;
         public DateTime DoB
         {
-            get { return _dob; }
-            set { _dob = value; OnPropertyChanged("DoB"); }
+            get { return dob; }
+            set { dob = value; OnPropertyChanged("DoB"); }
         }
         public bool CanSave => !string.IsNullOrEmpty(Email) &&
             !string.IsNullOrEmpty(FirstName) &&
@@ -143,7 +155,11 @@ namespace GoninDigital.ViewModels
         }
         private void load_page()
         {
-           user = DataProvider.Instance.Db.Users.Where(x => x.UserName == Settings.Default.usrname).First();
+            using (var db = new GoninDigitalDBContext())
+            {
+                user = db.Users.Where(x => x.UserName == Settings.Default.usrname).First();
+            }
+            
             Id = user.Id.ToString();
             userType = user.TypeId.ToString();
             switch (userType)
@@ -160,10 +176,12 @@ namespace GoninDigital.ViewModels
             }
             FirstName = user.FirstName.ToString();
             LastName = user.LastName.ToString();
+            FullName = FirstName + " " + LastName;
             PhoneNum =user.PhoneNumber.ToString();
             Email = user.Email.ToString();
             Gender = user.Gender.ToString();
             DoB =(DateTime) user.DateOfBirth;
+            UserName = user.UserName.ToString();
         }
         void SavePExecute()
         {
@@ -180,7 +198,12 @@ namespace GoninDigital.ViewModels
                 }
                 else
                 {
-                    if (Email != user.Email.ToString() & DataProvider.Instance.Db.Users.Where(x => x.Email == Email).Count() != 0)
+                    bool isEmail;
+                    using (var db = new GoninDigitalDBContext())
+                    {
+                        isEmail = db.Users.Where(x => x.Email == Email).Count() != 0;
+                    }
+                    if (Email != user.Email.ToString() & isEmail)
                     {
                         MessageBox.Show("Email already exists");
                     }
@@ -198,14 +221,18 @@ namespace GoninDigital.ViewModels
         }
         void UpdateInfo()
         {
-            var t = DataProvider.Instance.Db.Users.Where(x => x.UserName == Settings.Default.usrname).First();
-            t.Gender = byte.Parse(Gender);
-            t.FirstName = FirstName;
-            t.LastName=LastName;
-            t.PhoneNumber = PhoneNum;
-            t.Email = Email;
-            t.DateOfBirth = DoB;
-            _ = DataProvider.Instance.Db.SaveChanges();
+            using (var db = new GoninDigitalDBContext())
+            {
+                var t = db.Users.Where(x => x.UserName == Settings.Default.usrname).First();
+                t.Gender = byte.Parse(Gender);
+                t.FirstName = FirstName;
+                t.LastName = LastName;
+                t.PhoneNumber = PhoneNum;
+                t.Email = Email;
+                t.DateOfBirth = DoB;
+                _ = db.SaveChanges();
+            }
+            
             load_page();
         }
         #endregion
