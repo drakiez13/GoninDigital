cf41f7fbe55096e851d6f9db671d8aa246f9c1c1
Minh Phan <minh2442001@gmail.com>
2021-11-28 12:14:00 +0700

    done

--- Views/SplashScreen.xaml.cs
+++ Views/SplashScreen.xaml.cs
@@ -24,38 +24,6 @@ namespace GoninDigital.Views
         public SplashScreen()
         {
             InitializeComponent();
-            DataContext = ViewModel;
-        }
-        public ThreadedUIPageViewModel ViewModel { get; } = new ThreadedUIPageViewModel();
-
-        private void ProgressControlHost_ChildChanged(object sender, EventArgs e)
-        {
-            var host = (ThreadedVisualHost)sender;
-            if (host.Child is ThreadedProgressBar progressBar)
-            {
-                progressBar.SetBinding(ThreadedProgressBar.IsIndeterminateProperty, new Binding(nameof(ViewModel.IsBusy)) { Source = ViewModel });
-            }
-            else if (host.Child is ThreadedProgressRing progressRing)
-            {
-                progressRing.SetBinding(ThreadedProgressRing.IsActiveProperty, new Binding(nameof(ViewModel.IsBusy)) { Source = ViewModel });
-            }
-        }
-    }
-
-    public class ThreadedUIPageViewModel : BaseViewModel
-    {
-        private bool _isBusy = true;
-        public bool IsBusy
-        {
-            get => _isBusy;
-            set => Set(ref _isBusy, value);
-        }
-
-        private bool _isVisbile = true;
-        public bool IsVisible
-        {
-            get => _isVisbile;
-            set => Set(ref _isVisbile, value);
         }
     }
 }
