1006cfe1ca5be775c2bd1c785d725e8816783dfc
Thang Tran <drakiez92@gmail.com>
2021-11-29 12:20:04 +0700

    Sum the whole cart

---
+++ Converters/SumProductsPrice.cs
@@ -0,0 +1,33 @@
+﻿using GoninDigital.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+
+namespace GoninDigital.Converters
+{
+    internal class SumProductsPrice : IValueConverter
+    {
+        public object Convert(object values, Type targetType, object parameter, CultureInfo culture)
+        {
+            IEnumerable<Cart> selectedCarts = values as IEnumerable<Cart>;
+            long sum = 0;
+            foreach (var selected in selectedCarts)
+            {
+                sum += selected.Quantity * selected.Product.Price;
+            }
+                
+            return sum;
+        }
+
+        public object ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
