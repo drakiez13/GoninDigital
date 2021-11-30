0a9129da5b9c880e2792b9769fceb6b19c4459d6
C-Minh <sater5201@gmail.com>
2021-12-01 00:25:00 +0700

    myshop click edit remove

--- ViewModels/MyShopViewModel.cs
+++ ViewModels/MyShopViewModel.cs
@@ -9,12 +9,21 @@ using System.Windows;
 using GoninDigital.Models;
 using GoninDigital.Properties;
 using Microsoft.EntityFrameworkCore;
+using GoninDigital.SharedControl;
+using ModernWpf.Controls;
+using System.Windows.Input;
 
 namespace GoninDigital.ViewModels
 {
     class MyShopViewModel : BaseViewModel
     {
-        private Vendor vendor=null;
+        private Product selectedItem=null;
+        public Product SelectedItem
+        {
+            get { return selectedItem; }
+            set { selectedItem = value; OnPropertyChanged(); }
+        }
+        private Vendor vendor = null;
 
         public Vendor Vendor
         {
@@ -22,12 +31,15 @@ namespace GoninDigital.ViewModels
             set { vendor = value; OnPropertyChanged(); }
         }
 
-        private ObservableCollection<Product> products=null;
+        private ObservableCollection<Product> products = null;
         public ObservableCollection<Product> Products
         {
             get { return products; }
             set { products = value; OnPropertyChanged(); }
         }
+
+        public int OnPrimaryButtonClick { get; private set; }
+        public int PrimaryButtonClick { get; private set; }
 
         private void InitVendor()
         {
@@ -36,10 +48,10 @@ namespace GoninDigital.ViewModels
                 try
                 {
                     Vendor = db.Vendors.Include(o => o.Owner)
-                        .Include(o=>o.Products)
+                        .Include(o => o.Products)
                         .Single(o => o.Owner.UserName == Settings.Default.usrname);
                     /*Products = db.Products.Where(o => o.VendorId == Vendor.Id).ToList();*/
-                    Products=new ObservableCollection<Product>( Vendor.Products.ToList());
+                    Products = new ObservableCollection<Product>(Vendor.Products.ToList());
                 }
                 catch
                 {
@@ -50,15 +62,32 @@ namespace GoninDigital.ViewModels
         }
         public void OnNavigatedTo()
         {
-            
-        }
-        public void EditProduct()
-        {
 
+        }
+        public ICommand EditCommand { get; set; }
+        public void EditCommandExec(Product product)
+        {
+            SelectedItem = product;
+            var dialog = new ContentDialog
+            {
+                Content = new EditProductDialog(),
+
+                Title = "Title",
+                PrimaryButtonText = "Yes",
+                SecondaryButtonText = "No",
+                CloseButtonText = "Cancel",
+                DefaultButton = ContentDialogButton.Primary,
+                PrimaryButtonCommand = new RelayCommand<object>((p)=>true,p=>MessageBox.Show("sdsad")),
+                /*PrimaryButtonClick += OnPrimaryButtonClick,
+                SecondaryButtonClick += OnSecondaryButtonClick,
+                CloseButtonClick += OnCloseButtonClick,
+                Closed += OnClosed*/
+            };
+            dialog.ShowAsync();
         }
         public void RemoveProduct(Product product)
         {
-            using(var db= new GoninDigitalDBContext())
+            using (var db = new GoninDigitalDBContext())
             {
                 try
                 {
@@ -74,10 +103,10 @@ namespace GoninDigital.ViewModels
         }
         public MyShopViewModel()
         {
+            EditCommand = new RelayCommand<Product>(o => true, o => EditCommandExec(o));
             Thread thread = new Thread(InitVendor);
             thread.Start();
 
         }
-
     }
 }
