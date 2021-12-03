b6abf2b66928b943e9a15c9d76c21b935c4938d6
Thang Tran <drakiez92@gmail.com>
2021-12-03 12:29:56 +0700

    add constants

---
+++ Utils/Constants.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoninDigital.Utils
+{
+    static class Constants
+    {
+        public enum UserType
+        {
+            ADMIN = 1,
+            VENDOR = 2,
+            CUSTOMER = 3,
+        }
+
+        public enum GenderType
+        {
+            NOTKNOWN = 0,
+            MAKE = 1,
+            FEMALE = 2,
+            NOTAPPLICABLE = 9
+        }
+
+        public enum ProductStatus
+        {
+            CREATED = 1,
+            UPDATED = 2,
+            ACCEPTED =3,
+            REMOVED = 4,
+        }
+
+        public enum InvoiceStatus
+        {
+            CREATED = 1,
+            ACCEPTED = 2,
+            REFUSED = 3,
+            DELIVERED = 4,
+            CANCELED = 5
+        }
+    }
+}
