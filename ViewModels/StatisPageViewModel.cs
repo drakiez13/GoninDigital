d0ea4d6f352e6e02c415434f33aa7fe9aa308080
Minh Phan <minh2442001@gmail.com>
2021-12-20 17:23:02 +0700

    done

---
+++ ViewModels/StatisPageViewModel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using GoninDigital.Models;
+using GoninDigital.Properties;
+using LiveCharts;
+using LiveCharts.Wpf;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoninDigital.ViewModels
+{
+    internal class StatisPageViewModel : BaseViewModel
+    {
+        public StatisPageViewModel()
+        {
+            createdInvoices = new ObservableCollection<Invoice>();
+            acceptedInvoices = new ObservableCollection<Invoice>();
+            deliveredInvoices = new ObservableCollection<Invoice>();
+            canceledInvoices = new ObservableCollection<Invoice>();
+            refusedInvoices = new ObservableCollection<Invoice>();
+
+            Load_HistoryPurchase();
+
+            // Biểu đồ tròn
+            PointLabel = chartPoint => string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+
+
+
+            // Biểu đồ đường
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
+                },
+                new LineSeries
+                {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
+                    PointGeometry = null
+                },
+                new LineSeries
+                {
+                    Title = "Series 3",
+                    Values = new ChartValues<double> { 4,2,7,2,7 },
+                    PointGeometry = DefaultGeometries.Square,
+                    PointGeometrySize = 15
+                }
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            //modifying the series collection will animate and update the chart
+            SeriesCollection.Add(new LineSeries
+            {
+                Title = "Series 4",
+                Values = new ChartValues<double> { 5, 3, 2, 4 },
+                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
+                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
+                PointGeometrySize = 50,
+                PointForeground = Brushes.Gray
+            });
+
+            //modifying any series values will also animate and update the chart
+            SeriesCollection[3].Values.Add(5d);
+        }
+        public Func<ChartPoint, string> PointLabel { get; set; }
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+
+
+        private ObservableCollection<Invoice> createdInvoices;
+        public ObservableCollection<Invoice> CreatedInvoices
+        {
+            get { return createdInvoices; }
+            set { createdInvoices = value; OnPropertyChanged(); }
+        }
+        private ObservableCollection<Invoice> acceptedInvoices;
+        public ObservableCollection<Invoice> AcceptedInvoices
+        {
+            get { return acceptedInvoices; }
+            set { acceptedInvoices = value; OnPropertyChanged(); }
+        }
+        private ObservableCollection<Invoice> deliveredInvoices;
+        public ObservableCollection<Invoice> DeliveredInvoices
+        {
+            get { return deliveredInvoices; }
+            set { deliveredInvoices = value; OnPropertyChanged(); }
+        }
+        private ObservableCollection<Invoice> canceledInvoices;
+        public ObservableCollection<Invoice> CanceledInvoices
+        {
+            get { return canceledInvoices; }
+            set { canceledInvoices = value; OnPropertyChanged(); }
+        }
+        private ObservableCollection<Invoice> refusedInvoices;
+        public ObservableCollection<Invoice> RefusedInvoices
+        {
+            get { return refusedInvoices; }
+            set { refusedInvoices = value; OnPropertyChanged(); }
+        }
+        private void Load_HistoryPurchase()
+        {
+
+            using (var db = new GoninDigitalDBContext())
+            {
+                var userInvoices = db.Invoices.Include(o => o.Customer)
+                                              .Include(o => o.Vendor)
+                                              .Include(o => o.InvoiceDetails).ThenInclude(o => o.Product)
+                                              .Where(o => o.Customer.UserName == Settings.Default.usrname)
+                                              .ToList();
+                CreatedInvoices = new ObservableCollection<Invoice>(userInvoices.Where(o => o.StatusId == (int)Utils.Constants.InvoiceStatus.CREATED));
+                AcceptedInvoices = new ObservableCollection<Invoice>(userInvoices.Where(o => o.StatusId == (int)Utils.Constants.InvoiceStatus.ACCEPTED));
+                DeliveredInvoices = new ObservableCollection<Invoice>(userInvoices.Where(o => o.StatusId == (int)Utils.Constants.InvoiceStatus.DELIVERED));
+                CanceledInvoices = new ObservableCollection<Invoice>(userInvoices.Where(o => o.StatusId == (int)Utils.Constants.InvoiceStatus.CANCELED));
+                RefusedInvoices = new ObservableCollection<Invoice>(userInvoices.Where(o => o.StatusId == (int)Utils.Constants.InvoiceStatus.REFUSED));
+
+            }
+        }
+    }
+}
--- Views/DashBoardPages/StaticPage.xaml.cs
+++ Views/DashBoardPages/StaticPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GoninDigital.Models;
+using GoninDigital.ViewModels;
 using LiveCharts;
 using LiveCharts.Wpf;
 using Page = ModernWpf.Controls.Page;
@@ -26,56 +29,8 @@ namespace GoninDigital.Views.DashBoardPages
         public StaticPage()
         { 
             InitializeComponent();
-            PointLabel = chartPoint =>
-                string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
-
-            SeriesCollection = new SeriesCollection
-            {
-                new LineSeries
-                {
-                    Title = "Series 1",
-                    Values = new ChartValues<double> { 4, 6, 5, 2 ,4 }
-                },
-                new LineSeries
-                {
-                    Title = "Series 2",
-                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 },
-                    PointGeometry = null
-                },
-                new LineSeries
-                {
-                    Title = "Series 3",
-                    Values = new ChartValues<double> { 4,2,7,2,7 },
-                    PointGeometry = DefaultGeometries.Square,
-                    PointGeometrySize = 15
-                }
-            };
-
-            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
-            YFormatter = value => value.ToString("C");
-
-            //modifying the series collection will animate and update the chart
-            SeriesCollection.Add(new LineSeries
-            {
-                Title = "Series 4",
-                Values = new ChartValues<double> { 5, 3, 2, 4 },
-                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
-                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
-                PointGeometrySize = 50,
-                PointForeground = Brushes.Gray
-            });
-
-            //modifying any series values will also animate and update the chart
-            SeriesCollection[3].Values.Add(5d);
-
-            DataContext = this;
+            DataContext = new StatisPageViewModel();
         }
-        public Func<ChartPoint, string> PointLabel { get; set; }
-        public SeriesCollection SeriesCollection { get; set; }
-        public string[] Labels { get; set; }
-        public Func<double, string> YFormatter { get; set; }
-
-
         private void Chart_OnDataClick(object sender, ChartPoint chartpoint)
         {
             var chart = (LiveCharts.Wpf.PieChart)chartpoint.ChartView;
