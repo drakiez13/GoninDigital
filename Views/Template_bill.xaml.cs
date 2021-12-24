8334cc276f2c91467e8a608478b003b26e4b73dd
Minh Phan <minh2442001@gmail.com>
2021-12-25 02:49:04 +0700

    fuckkkkkkkkkkkkkkkkkkk

---
+++ Views/Template_bill.xaml.cs
@@ -0,0 +1,68 @@
+﻿using GoninDigital.Models;
+using GoninDigital.Properties;
+using GoninDigital.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GoninDigital.Views
+{
+    /// <summary>
+    /// Interaction logic for Template_bill.xaml
+    /// </summary>
+    public partial class Template_bill : Window
+    {
+        public Template_bill(Invoice x)
+        {
+            InitializeComponent();
+            DataContext = new export_bill(x);
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.IsEnabled = false;
+                PrintDialog printDialog = new PrintDialog();
+                if (printDialog.ShowDialog() == true)
+                {
+                    printDialog.PrintVisual(print, "invoice");
+                }
+            }
+            finally
+            {
+                this.IsEnabled = true;
+            }
+        }
+        
+    }
+    internal class export_bill : BaseViewModel
+    {
+        private ObservableCollection<Invoice> deliveredInvoices;
+        public ObservableCollection<Invoice> DeliveredInvoices
+        {
+            get { return deliveredInvoices; }
+            set { deliveredInvoices = value; OnPropertyChanged(); }
+        }
+        
+        public export_bill(Invoice x)
+        {
+            Load(x);
+        }
+        private async void Load(Invoice x)
+        {
+            DeliveredInvoices = new ObservableCollection<Invoice> { x };
+        }
+    }
+}
